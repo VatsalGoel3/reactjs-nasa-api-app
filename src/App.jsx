6257f6feb594402bd131d45db153c984543c973d
Vatsal Goel <144617902+VatsalGoel3@users.noreply.github.com>
2024-12-13 13:51:22 -0700

    Integrated NASA API: Set up secure API key storage using .env file and React's import.meta.env for access. Implemented data fetching with useEffect and async/await, successfully retrieving and logging data from the NASA APOD API.

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,7 @@
 import Sidebar from "./components/Sidebar"
 import Main from "./components/Main"
 import Footer from "./components/Footer"
-import { useState } from "react"
+import { useEffect, useState } from "react"
 
 function App() {
   const [showModal, setShowModal] = useState(false)
@@ -9,6 +9,24 @@ function App() {
   function handleToggleModal() {
     setShowModal(!showModal)
   }
+
+  useEffect(() => {
+    async function  fetchAPIData() {
+      const NASA_KEY = import.meta.env.VITE_NASA_API_KEY
+      const url = 'https://api.nasa.gov/planetary/apod' + 
+      `?api_key=${NASA_KEY}`
+      try {
+        const res = await fetch(url)
+        const data = await res.json()
+        console.log('DATA\n', data)
+      } catch (err) {
+        console.log(err.message)
+      }
+      
+    }
+    fetchAPIData()
+  }, [])
+
   return (
     <>
       <Main />
